20d6699da66598e8c3ee7d2a678d4f4d7b088dd5
BrenLau <blau4000@gmail.com>
2023-09-07 13:48:04 -0400

    hover animations

--- src/Deck/Deck.js
+++ src/Deck/Deck.js
@@ -2,7 +2,7 @@ import './deck.css'
 import { useEffect, useState } from 'react'
 import Card from '../Card/Card'
 
-const defaultDeck = ['AS', 'AD', 'AH', 'AC', '2S', '2D', '2H', '2C', '3S', '3H', '3C', '3D', '4S', '4H', '4C', '4D', '5S', '5H', '5C', '5D', '6S', '6H', '6C', '6D', '7S', '7H', '7C', '7D', '8S', '8H', '8C', '8D', '9S', '9H', '9C', '9D', 'TS', 'TH', 'TC', 'TD', 'JS', 'JH', 'JC', 'JD', 'QS', 'QH', 'QC', 'QD', 'KS', 'KH', 'KC', 'KD']
+const defaultDeck = ['AS', 'AH', 'AC', 'AD', '2S', '2H', '2C', '2D', '3S', '3H', '3C', '3D', '4S', '4H', '4C', '4D', '5S', '5H', '5C', '5D', '6S', '6H', '6C', '6D', '7S', '7H', '7C', '7D', '8S', '8H', '8C', '8D', '9S', '9H', '9C', '9D', 'TS', 'TH', 'TC', 'TD', 'JS', 'JH', 'JC', 'JD', 'QS', 'QH', 'QC', 'QD', 'KS', 'KH', 'KC', 'KD']
 
 let deck = []
 
@@ -27,8 +27,14 @@ const Deck = ({ deckCount }) => {
 
     return (
         <>
-            <Card card={'KC'} />
             <div>{currentDeck.length}</div>
+            {defaultDeck.map((card) => {
+
+                return (
+                    <Card card={card} />
+                )
+            }
+            )}
         </>
     )
 }
