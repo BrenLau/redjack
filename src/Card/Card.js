2c8d254b3ba616c24db41c1112e85eb94122a162
BrenLau <blau4000@gmail.com>
2023-09-07 20:10:11 -0400

    decrement cards

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -3,10 +3,18 @@ import spade from './suits/spade.png'
 import heart from './suits/heart.png'
 import club from './suits/club.png'
 import diamond from './suits/diamond.png'
+import { useState } from 'react'
 
 const Card = ({ card, cardCounter }) => {
+    const [cardCount, setCardCount] = useState(cardCounter)
     return (
-        <div className='card'>
+        <div className='card' onClick={(e) => {
+            if (cardCount[card] > 0) {
+                let currentCards = { ...cardCount }
+                currentCards[card] -= 1
+                setCardCount(currentCards)
+            }
+        }}>
 
             <div className='topline'>{
                 card[0] === 'T' ? '10' : card[0]
@@ -15,7 +23,7 @@ const Card = ({ card, cardCounter }) => {
             <img className='suit' src={
                 card[1] === 'S' ? spade : card[1] === 'H' ? heart : card[1] === 'C' ? club : diamond
             }></img>
-            <div className='botline'>x{cardCounter[card]}</div>
+            <div className='botline'>x{cardCount[card]}</div>
 
         </div >
     )
